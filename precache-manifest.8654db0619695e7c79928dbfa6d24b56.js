193964a6dd06b0882accfd48726a540cc404bef9
melissa <nadia.mm.mariduena@gmail.com>
2020-09-16 23:21:29 +0200

    Updates

---
+++ precache-manifest.8654db0619695e7c79928dbfa6d24b56.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38cce17ce4ca9fe8ebadd8f8020b946",
+    "url": "/react-imgreveal-mini-portfolio1/index.html"
+  },
+  {
+    "revision": "6b9cfb4b93a6bfa8cf44",
+    "url": "/react-imgreveal-mini-portfolio1/static/css/main.c3a680d7.chunk.css"
+  },
+  {
+    "revision": "98fa047e2b394e2c5bc6",
+    "url": "/react-imgreveal-mini-portfolio1/static/js/2.f12101de.chunk.js"
+  },
+  {
+    "revision": "6341ee539f4c2fb843b74bf976b5fdff",
+    "url": "/react-imgreveal-mini-portfolio1/static/js/2.f12101de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9cfb4b93a6bfa8cf44",
+    "url": "/react-imgreveal-mini-portfolio1/static/js/main.600dc610.chunk.js"
+  },
+  {
+    "revision": "8d56fa78e84590e2780e",
+    "url": "/react-imgreveal-mini-portfolio1/static/js/runtime-main.6280fa31.js"
+  },
+  {
+    "revision": "a60638237a8eca001cd56ad1e4c63e37",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Arimo-Bold.a6063823.ttf"
+  },
+  {
+    "revision": "9fe23bf225eecfcdb736e063b08a6123",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Arimo-Regular.9fe23bf2.ttf"
+  },
+  {
+    "revision": "cdbdde62fc0bd342a83a7caa49b8b112",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/BungeeHairline-Regular.cdbdde62.ttf"
+  },
+  {
+    "revision": "f33327cdc7c1c759c638ac5860736cbd",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/CutiveMono-Regular.f33327cd.ttf"
+  },
+  {
+    "revision": "3f6edbad564da8dd40da2c35001a7ae6",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/FjordOne-Regular.3f6edbad.ttf"
+  },
+  {
+    "revision": "43acdd46beda5bcd20313cc5ad7e524e",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/FrankRuhlLibre-Black.43acdd46.ttf"
+  },
+  {
+    "revision": "cca21143c5a15f71eeddb6b07a91f571",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/FrankRuhlLibre-Light.cca21143.ttf"
+  },
+  {
+    "revision": "d222d5bd20fce943e43d2aa3b85b1eb0",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Jura-Bold.d222d5bd.ttf"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "9a77fbaa85fa42b73e3b96399daf49c5",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Lato-Thin.9a77fbaa.ttf"
+  },
+  {
+    "revision": "44ea8275e878ed72a9c8a393f9de12d2",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Muli-Bold.44ea8275.ttf"
+  },
+  {
+    "revision": "b0a60ec1073a03679d8a3ddbfaa617c0",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Muli-BoldItalic.b0a60ec1.ttf"
+  },
+  {
+    "revision": "17c1038fa865fea142dfd68676518105",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Muli-SemiBoldItalic.17c1038f.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "d81f6a473fc85da9e69f0737619596f0",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Rajdhani-Light.d81f6a47.ttf"
+  },
+  {
+    "revision": "fb987593d286bb6bd93d3de8457c5eb4",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Rajdhani-Regular.fb987593.ttf"
+  },
+  {
+    "revision": "f63c2b055b95c371055b21296cbfef59",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Rajdhani-SemiBold.f63c2b05.ttf"
+  },
+  {
+    "revision": "466d154fedd98f85c9deb363ccf859a7",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Raleway-Light.466d154f.ttf"
+  },
+  {
+    "revision": "5faedfece17998f456bf5b32b100b597",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Raleway-Thin.5faedfec.ttf"
+  },
+  {
+    "revision": "3234b3e853eabe9fc936cd7a133055ad",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Roboto-Black.3234b3e8.ttf"
+  },
+  {
+    "revision": "fee6559350dca73f2208b02ac28a113e",
+    "url": "/react-imgreveal-mini-portfolio1/static/media/Tom_van_der_Borght_metalmagazine-19.fee65593.jpg"
+  }
+]);
